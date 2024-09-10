2c7b76496ac1f65eeb3002c49356b6214ee0869e
Danilo Mourelle <danilomourelle@outlook.com>
2024-09-10 13:04:37 -0300

    build(dayjs):install dayjs and update seed to change dates

--- database/seed.ts
+++ database/seed.ts
@@ -1,5 +1,6 @@
 import { client, db } from "../src/database";
 import { goalCompletions, goals } from "./schema";
+import dayjs from 'dayjs';
 
 async function seed() {
 	await db.delete(goalCompletions);
@@ -11,10 +12,12 @@ async function seed() {
 		{ title: "Ler", desiredWeeklyFrequency: 2 },
 	]).returning();
 
+  const startOfWeek = dayjs().startOf('week')
+
   await db.insert(goalCompletions).values([
-    { goalId: result[0].id, createdAd: new Date() },
-    { goalId: result[1].id, createdAd: new Date() },
-    { goalId: result[2].id, createdAd: new Date() },
+    { goalId: result[0].id, createdAd: startOfWeek.toDate() },
+    { goalId: result[1].id, createdAd: startOfWeek.add(1, 'day').toDate() },
+    { goalId: result[2].id, createdAd: startOfWeek.toDate() },
   ]);
 }
 
